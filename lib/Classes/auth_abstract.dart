a11cfea40b2406ab2d94936c9bd70a13abdcf695
webs127 <davidshittu20@gmail.com>
2022-10-02 16:34:01 -0700

    New Changes

--- lib/Classes/auth_abstract.dart
+++ lib/Classes/auth_abstract.dart
@@ -1,13 +1,25 @@
 abstract class FireBaseAuthentication {
-  Future signInWithEmailAndPassword({required String email, required String password});
-  Future registerWithEmailAndPassword({required String email, required String password, required String firstName,
+  //Sign in user with existing email and password
+  Future signInWithEmailAndPassword(
+      {required String email, required String password});
+
+  //Create new user with email and password
+  Future registerWithEmailAndPassword({
+    required String email,
+    required String password,
+    required String firstName,
     required String lastName,
     required String gender,
     required int age,
     required String phoneNumber,
     required String country,
     required String state,
-    required String address,});
+    required String address,
+  });
+
+  //Sign out user
   Future signOut();
+
+  //Stream for listening for user changes
   Stream getUserChanges();
 }
